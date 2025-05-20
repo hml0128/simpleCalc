7501f74c7c8d566b79c4668170dc9cd394145077
규범 <kimhml0128@gmail.com>
2025-05-20 09:52:32 +0900

    num2 0인 경우 제거

--- main.py
+++ main.py
@@ -47,7 +47,8 @@ while True:
             print(num1, "*", num2, "=", multiply(num1, num2))
             
         elif choice =='4':
-            print(num1, "/", num2, "=", divide(num1,num2))
+            if num2 != 0:
+                 print(num1, "/", num2, "=", divide(num1,num2))
             
 
         # check if user wants another calculation
